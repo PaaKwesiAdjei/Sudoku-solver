26d9cc8eb0b6006571eb7e20bf87d190b7d05569
Kelvin Adjei <kpkadjei1@gmail.com>
2021-02-03 23:19:54 -0500

    Mistakes resolved comments left
    Not done commenting code

--- GUI.py
+++ GUI.py
@@ -1,32 +1,31 @@
-# GUI.py
-import os
+"""GUI implementation written by Kelvin Adjei"""
 import pygame
+from solveOriginal import solve, valid
 import time
 pygame.font.init()
 
+
 class Grid:
     board = [
-            [7, 8, 0, 4, 0, 0, 1, 2, 0],
-            [6, 0, 0, 0, 7, 5, 0, 0, 9],
-            [0, 0, 0, 6, 0, 1, 0, 7, 8],
-            [0, 0, 7, 0, 4, 0, 2, 6, 0],
-            [0, 0, 1, 0, 5, 0, 9, 3, 0],
-            [9, 0, 4, 0, 6, 0, 0, 0, 5],
-            [0, 7, 0, 3, 0, 0, 0, 1, 2],
-            [1, 2, 0, 0, 0, 7, 4, 0, 0],
-            [0, 4, 9, 2, 0, 6, 0, 0, 7],
-        ]
+        [7, 8, 0, 4, 0, 0, 1, 2, 0],
+        [6, 0, 0, 0, 7, 5, 0, 0, 9],
+        [0, 0, 0, 6, 0, 1, 0, 7, 8],
+        [0, 0, 7, 0, 4, 0, 2, 6, 0],
+        [0, 0, 1, 0, 5, 0, 9, 3, 0],
+        [9, 0, 4, 0, 6, 0, 0, 0, 5],
+        [0, 7, 0, 3, 0, 0, 0, 1, 2],
+        [1, 2, 0, 0, 0, 7, 4, 0, 0],
+        [0, 4, 9, 2, 0, 6, 0, 0, 7]
+    ]
 
-    def __init__(self, rows, cols, width, height, win):
+    def __init__(self, rows, cols, width, height):
         self.rows = rows
         self.cols = cols
         self.cubes = [[Cube(self.board[i][j], i, j, width, height) for j in range(cols)] for i in range(rows)]
         self.width = width
         self.height = height
         self.model = None
-        self.update_model()
         self.selected = None
-        self.win = win
 
     def update_model(self):
         self.model = [[self.cubes[i][j].value for j in range(self.cols)] for i in range(self.rows)]
@@ -37,7 +36,7 @@ class Grid:
             self.cubes[row][col].set(val)
             self.update_model()
 
-            if valid(self,model, val, (row,col)) and self.solve():
+            if valid(self.model, val, (row,col)) and solve(self.model):
                 return True
             else:
                 self.cubes[row][col].set(0)
@@ -49,21 +48,21 @@ class Grid:
         row, col = self.selected
         self.cubes[row][col].set_temp(val)
 
-    def draw(self):
+    def draw(self, win):
         # Draw Grid Lines
         gap = self.width / 9
-        for i in range(self.rows + 1):
-            if i%3 == 0 and i != 0:
+        for i in range(self.rows+1):
+            if i % 3 == 0 and i != 0:
                 thick = 4
             else:
                 thick = 1
-            pygame.draw.line(self.win, (0,0,0),(0, i*gap),(self.width, i*gap), thick)
-            pygame.draw.line(self.win, (0,0,0),(i*gap, 0),(i*gap, self.height), thick)
+            pygame.draw.line(win, (0,0,0), (0, i*gap), (self.width, i*gap), thick)
+            pygame.draw.line(win, (0, 0, 0), (i * gap, 0), (i * gap, self.height), thick)
 
         # Draw Cubes
         for i in range(self.rows):
             for j in range(self.cols):
-                self.cubes[i][j].draw(self.win)
+                self.cubes[i][j].draw(win)
 
     def select(self, row, col):
         # Reset all other
@@ -81,8 +80,8 @@ class Grid:
 
     def click(self, pos):
         """
-        Parameter: pos
-        Return: (row, col)
+        :param: pos
+        :return: (row, col)
         """
         if pos[0] < self.width and pos[1] < self.height:
             gap = self.width / 9
@@ -99,57 +98,12 @@ class Grid:
                     return False
         return True
 
-    def solve(self):
-        find = find_empty(self.model)
-        if not find:
-            return True
-        else:
-            row, col = find
-
-        for i in range(1, 10):
-            if valid(self.model, i, (row, col)):
-                self.model[row][col] = i
-
-                if self.solve():
-                    return True
-
-                self.model[row][col] = 0
-
-        return False
-
-    def solve_gui(self):
-        find = find_empty(self.model)
-        if not find:
-            return True
-        else:
-            row, col = find
-        for i in range(1, 10):
-                if valid(self.model, i, (row, col)):
-                    self.model[row][col] = i
-                    self.cubes[row][col].set(i)
-                    self.cubes[row][col].draw_change(self.win, True)
-                    self.update_model()
-                    pygame.display.update()
-                    pygame.time.delay(100)
-
-                    if self.solve_gui():
-                        return True
-
-                    self.model[row][col] = 0
-                    self.cubes[row][col].set(0)
-                    self.update_model()
-                    self.cubes[row][col].draw_change(self.win, False)
-                    pygame.display.update()
-                    pygame.time.delay(100)
-
-        return False
-
 
 class Cube:
     rows = 9
     cols = 9
 
-    def __init__(self, value, row, col, width, height):
+    def __init__(self, value, row, col, width ,height):
         self.value = value
         self.temp = 0
         self.row = row
@@ -175,22 +129,6 @@ class Cube:
         if self.selected:
             pygame.draw.rect(win, (255,0,0), (x,y, gap ,gap), 3)
 
-    def draw_change(self, win, g=True):
-        fnt = pygame.font.SysFont("comicsans", 40)
-
-        gap = self.width / 9
-        x = self.col * gap
-        y = self.row * gap
-
-        pygame.draw.rect(win, (255, 255, 255), (x, y, gap, gap), 0)
-
-        text = fnt.render(str(self.value), 1, (0, 0, 0))
-        win.blit(text, (x + (gap / 2 - text.get_width() / 2), y + (gap / 2 - text.get_height() / 2)))
-        if g:
-            pygame.draw.rect(win, (0, 255, 0), (x, y, gap, gap), 3)
-        else:
-            pygame.draw.rect(win, (255, 0, 0), (x, y, gap, gap), 3)
-
     def set(self, val):
         self.value = val
 
@@ -198,65 +136,32 @@ class Cube:
         self.temp = val
 
 
-    def find_empty(bo):
-        for i in range(len(bo)):
-            for j in range(len(bo[0])):
-                if bo[i][j] == 0:
-                    return (i, j)  # row, col
-
-        return None
-
-
-    def valid(bo, num, pos):
-        # Check row
-        for i in range(len(bo[0])):
-            if bo[pos[0]][i] == num and pos[1] != i:
-                return False
-
-        # Check column
-        for i in range(len(bo)):
-            if bo[i][pos[1]] == num and pos[0] != i:
-                return False
-
-        # Check box
-        box_x = pos[1] // 3
-        box_y = pos[0] // 3
-
-        for i in range(box_y*3, box_y*3 + 3):
-            for j in range(box_x * 3, box_x*3 + 3):
-                if bo[i][j] == num and (i,j) != pos:
-                    return False
-
-        return True
+def redraw_window(win, board, time, strikes):
+    win.fill((255,255,255))
+    # Draw time
+    fnt = pygame.font.SysFont("comicsans", 40)
+    text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
+    win.blit(text, (540 - 160, 560))
+    # Draw Strikes
+    text = fnt.render("X " * strikes, 1, (255, 0, 0))
+    win.blit(text, (20, 560))
+    # Draw grid and board
+    board.draw(win)
 
 
-    def redraw_window(win, board, time, strikes):
-        win.fill((255,255,255))
-        # Draw time
-        fnt = pygame.font.SysFont("comicsans", 40)
-        text = fnt.render("Time: " + format_time(time), 1, (0,0,0))
-        win.blit(text, (540 - 160, 560))
-        # Draw Strikes
-        text = fnt.render("X " * strikes, 1, (255, 0, 0))
-        win.blit(text, (20, 560))
-        # Draw grid and board
-        board.draw()
+def format_time(secs):
+    sec = secs%60
+    minute = secs//60
+    hour = minute//60
 
-
-    def format_time(secs):
-        sec = secs%60
-        minute = secs//60
-        hour = minute//60
-
-        mat = " " + str(minute) + ":" + str(sec)
-        return mat
+    mat = " " + str(minute) + ":" + str(sec)
+    return mat
 
 
 def main():
-    os.environ['SDL_VIDEODRIVER'] = 'dummy'
     win = pygame.display.set_mode((540,600))
     pygame.display.set_caption("Sudoku")
-    board = Grid(9, 9, 540, 540, win)
+    board = Grid(9, 9, 540, 540)
     key = None
     run = True
     start = time.time()
@@ -290,10 +195,6 @@ def main():
                 if event.key == pygame.K_DELETE:
                     board.clear()
                     key = None
-
-                if event.key == pygame.K_SPACE:
-                    board.solve_gui()
-
                 if event.key == pygame.K_RETURN:
                     i, j = board.selected
                     if board.cubes[i][j].temp != 0:
@@ -306,6 +207,7 @@ def main():
 
                         if board.is_finished():
                             print("Game over")
+                            run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
--- solveOriginal.py
+++ solveOriginal.py
@@ -1,21 +1,23 @@
+# solveOriginal.py written by: Kelvin Adjei
+
 import pprint
 
-#solver.py
-def solve(bo):
-    """
+"""
     description: Solves sudoku board using backtracking
     parameters: bo, 2d list of ints
     return: solution
 
-    """
-    find = find_empty(bo)
-    if find:
-        row,col = find
-    else:
+"""
+def solve(bo):
+    #finds points in board without entries
+    find = find_empty(bo) 
+    if not find:
         return True
+    else:
+        row, col = find
 
     for i in range(1,10):
-        if valid(bo, (row, col), i):
+        if valid(bo, i, (row, col)):
             bo[row][col] = i
 
             if solve(bo):
@@ -25,64 +27,53 @@ def solve(bo):
 
     return False
 
-def valid(bo, pos, num):
-    """
-    description: Returns if the attempted move is valid
-    parameters: bo, 2d list of ints
-                pos, (row, col)
-                num: int
-    """
 
-    #Check row
-    for i in range(0, len(bo)):
-        if bo[pos[0]][i] == num and pos[1] != i:
+def valid(bo, num, pos):
+    # Check row
+    for i in range(len(bo[0])):
+        if (bo[pos[0]][i] == num and pos[1] != i):
             return False
 
-    #Check Col
-    for i in range(0, len(bo)):
-        if bo[i][pos[1]] == num and pos[1] != i:
+    # Check column
+    for i in range(len(bo)):
+        if bo[i][pos[1]] == num and pos[0] != i:
             return False
 
-    #Check box
-    box_x = pos[1]//3
-    box_y = pos[0]//3
+    # Check box
+    box_x = pos[1] // 3
+    box_y = pos[0] // 3
+    
 
-    for i in range(box_y*3,box_y*3 + 3):
-        for j in range(box_x*3,box_x*3 +3):
+    for i in range(box_y*3, box_y*3 + 3):
+        for j in range(box_x * 3, box_x*3 + 3):
             if bo[i][j] == num and (i,j) != pos:
                 return False
 
     return True
 
-def find_empty(bo):
-    """
-    description: finds an empty space in the board
-    parameters: bo, partially complete board
-    return: (int, int), row col
-    """
 
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
+
+def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return(i, j)
+                return (i, j)  # row, col
 
     return None
-
-def print_board(bo):
-    """
-    description: prints the board
-    parameters: bo,2d list of ints
-    return: home
-
-    """
-    for i in range(len(bo)):
-        if i%3 == 0 and i != 0:
-            print('- - - - - - - - - - - - - - -')
-        for j in range(len(bo[0])):
-            if j%3 == 0:
-                print(" | ",end="\n")
-            else:
-                print(str(bo[i][j]) + " ", end="")
 
 board = [
         [7, 8, 0, 4, 0, 0, 1, 2, 0],
